55d97796e85ce988cb81137fe4aa0a90f2a9f1a0
srinathreddy <rsrinath34@gmail.com>
2022-04-17 01:59:31 +0530

    new commit

--- frontend/src/mycomponents/student/Registercomp.js
+++ frontend/src/mycomponents/student/Registercomp.js
@@ -1,72 +1,139 @@
-import React from 'react'
+// import React from 'react'
 import {Container} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Registercomp() {
-  return (
-                <div className='main'>
-                <Container className='registerconatiner'> 
-                <div className='sub-main'>
-                    <h5>Student Registration</h5>
-                    <div className='registerform'>
-                    <div className=" usn mb-3 row">
-                        <label htmlFor="usn" class="col-sm-2 col-form-label">USN</label>
-                        <div className="col-sm-10">
-                            <input type="text" class=" text1 form-control rounded-pill" id="usn"/>
-                        </div>
-                    </div>
-                    <div className=" name mb-3 row">
-                        <label htmlFor="name" class="col-sm-2 col-form-label">Name</label>
-                        <div className="col-sm-10">
-                            <input type="text" class=" text1 form-control rounded-pill" id="name"/>
-                        </div>
-                    </div>
-                    <div className=" sem mb-3 row">
-                        <label htmlFor="sem" class="col-sm-2 col-form-label">Sem</label>
-                        <div className="col-sm-10">
-                            <input type="number" min='1' max='8' class=" text1 form-control rounded-pill" id="name"/>
-                        </div>
-                    </div>
-                    <div className="branch mb-3 row">
-                        <label htmlFor="branch" class="col-sm-2 col-form-label">Branch</label>
-                        <select name='branch' className='branchname' >
-                            <option value='cse'>CSE</option>
-                            <option value='ise'>ISE</option>
-                            <option value='ece'>ECE</option>
-                            <option value='mec'>MEC</option>
-                            <option value='EEE'>EEE</option>
-                            <option value='civil'>CIVIL</option>
-                        </select>
-                    </div>
-                    <div className=" dob mb-3 row">
-                        <label htmlFor="dob" class="col-sm-2 col-form-label">DOB</label>
-                        <div className="col-sm-10">
-                            <input type="date" class=" text1 form-control rounded-pill" id="name"/>
-                        </div>
-                    </div>
-                    <div className=" mobile mb-3 row">
-                        <label htmlFor="mobile" class="col-sm-2 col-form-label">Mobile</label>
-                        <div className="col-sm-10">
-                            <input type="tel" class=" text1 form-control rounded-pill" id="name"/>
-                        </div>
-                    </div>
-                    <div className=" parents mb-3 row">
-                        <label htmlFor="parents" class="col-sm-2 col-form-label">Parents Mobile</label>
-                        <div className="col-sm-10">
-                            <input type="tel" class=" text1 form-control rounded-pill" id="name"/>
-                        </div>
-                    </div>
-                    <div className=" parents mb-3 row">
-                        <label htmlFor="password" class="col-sm-2 col-form-label">Password</label>
-                        <div className="col-sm-10">
-                            <input type="password" class=" text1 form-control rounded-pill" id="name"/>
-                        </div>
-                    </div>
-                    <button type="button" class=" submitbtn btn btn-primary">Submit</button>
 
-                    </div>
-                </div>
-                </Container>
+    const [usn, setUSN] = useState();
+    const [name, setName] = useState();
+    const [sem, setSem] = useState();
+    const [branch, setBranch] = useState();
+    const [dob, setDob] = useState();
+    const [mobile, setMobile] = useState();
+    const [parentsmobile, setParentsmobile] = useState();
+    const [password, setPassword] = useState();
+    // const [gender, setGender] = useState();
+    const [address, setAddress] = useState();
+  
+    const navigate = useNavigate()
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+      console.log(usn,password,sem,branch,dob,mobile,parentsmobile,password);
+      const user = {
+          USN:usn,
+          name:name,
+          branch:branch,
+          sem:sem,
+          subject:[],
+          dob:dob,
+          contactInfo:{
+              address:address,
+              mobileNo:mobile,
+              parentMobileNo:parentsmobile
+          },
+          password:password
+      };
+      let axiosConfig = {
+          headers: {
+              'Content-Type': 'application/json;charset=UTF-8',
+              "Access-Control-Allow-Origin": "localhost:3000",
+          }
+        };
+        axios.post('http://localhost:3000/studentRegistration',  user , axiosConfig)
+            .then(res => {
+              console.log(res.data);
+              if(res.data.data.length==0){
+              //   handleClickToOpen();
+              }else{
+                navigate('/');
+              }
+            
+              
+            })
+  }
+  return (
+    <div className='main'>
+    <Container className='registerconatiner'> 
+    <div className='sub-main'>
+        <h5>Student Registration</h5>
+        <div className='registerform'>
+        <div className=" usn mb-3 row">
+            <label htmlFor="usn" class="col-sm-2 col-form-label">USN</label>
+            <div className="col-sm-10">
+                <input type="text" class=" text1 form-control rounded-pill" onChange={e=>setUSN(e.target.value)} id="usn"/>
             </div>
+        </div>
+        <div className=" name mb-3 row">
+            <label htmlFor="name" class="col-sm-2 col-form-label">Name</label>
+            <div className="col-sm-10">
+                <input type="text" class=" text1 form-control rounded-pill" onChange={e=>setName(e.target.value)} id="name"/>
+            </div>
+        </div>
+        <div className=" sem mb-3 row">
+            <label htmlFor="sem" class="col-sm-2 col-form-label">Sem</label>
+            <div className="col-sm-10">
+                <input type="number" min='1' max='8' class=" text1 form-control rounded-pill" onChange={e=>setSem(e.target.value)} id="name"/>
+            </div>
+        </div>
+        <div className="branch mb-3 row">
+            <label htmlFor="branch" class="col-sm-2 col-form-label">Branch</label>
+            <select name='branch' className='branchname' onChange={e=>setBranch(e.target.value)} >
+                <option value='cse'>CSE</option>
+                <option value='ise'>ISE</option>
+                <option value='ece'>ECE</option>
+                <option value='mec'>MEC</option>
+                <option value='EEE'>EEE</option>
+                <option value='civil'>CIVIL</option>
+            </select>
+        </div>
+        <div className=" dob mb-3 row">
+            <label htmlFor="dob" class="col-sm-2 col-form-label">DOB</label>
+            <div className="col-sm-10">
+                <input type="date" class=" text1 form-control rounded-pill" onChange={e=>setDob(e.target.value)} id="name"/>
+            </div>
+        </div>
+        <div className=" gender mb-3 row" >
+        <label htmlFor="gender" class="col-sm-2 col-form-label">Gender</label>
+            <div className="col-2">
+            <input type="radio" value="Male" name="gender"  /> Male
+            </div>
+            <div className="col-2">
+            <input type="radio" value="Female" name="gender" /> Female
+            </div>
+        </div>
+        <div className=" address mb-3 row">
+            <label htmlFor="address" class="col-sm-2 col-form-label">Address</label>
+            <div className="col-sm-10">
+                <textarea id='address' name='address' className='addressfield' onChange={e=>setAddress(e.target.value)} ></textarea>
+            </div>
+        </div>
+        <div className=" mobile mb-3 row">
+            <label htmlFor="mobile" class="col-sm-2 col-form-label">Mobile</label>
+            <div className="col-sm-10">
+                <input type="tel" class=" text1 form-control rounded-pill" onChange={e=>setMobile(e.target.value)} id="name"/>
+            </div>
+        </div>
+        <div className=" parents mb-3 row">
+            <label htmlFor="parents" class="col-sm-2 col-form-label">Parents Mobile</label>
+            <div className="col-sm-10">
+                <input type="tel" class=" text1 form-control rounded-pill" onChange={e=>setParentsmobile(e.target.value)} id="name"/>
+            </div>
+        </div>
+        <div className=" parents mb-3 row">
+            <label htmlFor="password" class="col-sm-2 col-form-label">Password</label>
+            <div className="col-sm-10">
+                <input type="password" class=" text1 form-control rounded-pill" onChange={e=>setPassword(e.target.value)} id="name"/>
+            </div>
+        </div>
+        <button type="button" class=" submitbtn btn btn-primary" onClick={handleSubmit}>Submit</button>
+
+        </div>
+    </div>
+    </Container>
+</div>
   )
 }
 
--- frontend/src/mycomponents/teacher/Registercomp.js
+++ frontend/src/mycomponents/teacher/Registercomp.js
@@ -1,7 +1,62 @@
-import React from 'react'
+// import React from 'react'
 import {Container} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Registercomp() {
+    const [employeeid, setEmployeeId] = useState();
+    const [name, setName] = useState();
+    const [department, setDepartment] = useState();
+    const [dob, setDob] = useState();
+    const [gender, setGender] = useState();
+    const [address, setAddress] = useState();
+    const [mobile, setMobile] = useState();
+    const [subteacher, setSubjectTeacher] = useState();
+    const [password, setPassword] = useState();
+
+    const navigate = useNavigate()
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // console.log(usn,password,sem,branch,dob,mobile,parentsmobile,password);
+        const user = {
+            EmployeeId:employeeid,
+            name:name,
+            sem:6,
+            Dep:department,
+            gender:gender,
+            subject:[],
+            dob:dob,
+            contactInfo:{
+                address:address,
+                mobileNo:mobile,
+                parentMobileNo:7418529630
+            },
+            role:subteacher,
+            password:password
+        }
+
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "localhost:3000",
+            }
+          };
+
+          axios.post('http://localhost:3000/studentRegistration',  user , axiosConfig)
+          .then(res => {
+            console.log(res.data);
+            if(res.data.data.length==0){
+            //   handleClickToOpen();
+            }else{
+              navigate('/');
+            }
+          
+            
+          })  
+
+}
   return (
                 <div className='main'>
                 <Container className='registerconatiner'> 
@@ -12,18 +67,18 @@ function Registercomp() {
                     <div className=" usn mb-3 row">
                         <label htmlFor="usn" class="col-sm-2 col-form-label">EmployeeId</label>
                         <div className="col-sm-10">
-                            <input type="text" class=" text1 form-control rounded-pill" id="usn"/>
+                            <input type="text" class=" text1 form-control rounded-pill" onChange={e=>setEmployeeId(e.target.value)} id="usn"/>
                         </div>
                     </div>
                     <div className=" name mb-3 row">
                         <label htmlFor="name" class="col-sm-2 col-form-label">Name</label>
                         <div className="col-sm-10">
-                            <input type="text" class=" text1 form-control rounded-pill" id="name"/>
+                            <input type="text" class=" text1 form-control rounded-pill" onChange={e=>setName(e.target.value)} id="name"/>
                         </div>
                     </div>
                     <div className="branch mb-3 row">
                         <label htmlFor="branch" class="col-sm-2 col-form-label">Dep</label>  
-                        <select name='branch' className='branchname' >
+                        <select name='branch' className='branchname' onChange={e=>setDepartment(e.target.value)} >
                             <option value='cse'>CSE</option>
                             <option value='ise'>ISE</option>
                             <option value='ece'>ECE</option>
@@ -35,43 +90,43 @@ function Registercomp() {
                     <div className=" dob mb-3 row">
                         <label htmlFor="dob" class="col-sm-2 col-form-label">DOB</label>
                         <div className="col-sm-10">
-                            <input type="date" class=" text1 form-control rounded-pill" id="name"/>
+                            <input type="date" class=" text1 form-control rounded-pill" onChange={e=>setDob(e.target.value)} id="name"/>
                         </div>
                     </div>
                     <div className=" dob mb-3 row" >
                     <label htmlFor="dob" class="col-sm-2 col-form-label">Gender</label>
                         <div className="col-2">
-                        <input type="radio" value="Male" name="gender" /> Male
+                        <input type="radio" value="Male" name="gender" onChange={e=>setGender(e.target.value)} /> Male
                         </div>
                         <div className="col-2">
-                        <input type="radio" value="Female" name="gender" /> Female
+                        <input type="radio" value="Female" name="gender" onChange={e=>setGender(e.target.value)} /> Female
                         </div>
                     </div>
                     <div className=" name mb-3 row">
                         <label htmlFor="name" class="col-sm-2 col-form-label">Address</label>
                         <div className="col-sm-10">
-                            <textarea rows = "2" cols = "60" class=" text1 form-control " id="name"/>
+                            <textarea rows = "2" cols = "60" class=" text1 form-control " onChange={e=>setAddress(e.target.value)} id="name"/>
                         </div>
                     </div>
                     <div className=" mobile mb-3 row">
                         <label htmlFor="mobile" class="col-sm-2 col-form-label">Mobile</label>
                         <div className="col-sm-10">
-                            <input type="tel" class=" text1 form-control rounded-pill" id="name"/>
+                            <input type="tel" class=" text1 form-control rounded-pill" onChange={e=>setMobile(e.target.value)} id="name"/>
                         </div>
                     </div>
                     <div className=" name mb-3 row">
                         <label htmlFor="name" class="col-sm-2 col-form-label">Subject Teach</label>
                         <div className="col-sm-10">
-                            <textarea rows = "2" cols = "60" class=" text1 form-control " id="name"/>
+                            <textarea rows = "2" cols = "60" class=" text1 form-control " onChange={e=>setSubjectTeacher(e.target.value)} id="name"/>
                         </div>
                     </div>
                     <div className=" parents mb-3 row">
                         <label htmlFor="password" class="col-sm-2 col-form-label">Password</label>
                         <div className="col-sm-10">
-                            <input type="password" class=" text1 form-control rounded-pill" id="name"/>
+                            <input type="password" class=" text1 form-control rounded-pill" onChange={e=>setPassword(e.target.value)} id="name"/>
                         </div>
                     </div>
-                    <button type="button" class=" submitbtn btn btn-primary">Submit</button>
+                    <button type="button" class=" submitbtn btn btn-primary" onClick={handleSubmit}>Submit</button>
 
                     </div>
                 </div>
